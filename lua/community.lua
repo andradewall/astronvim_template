16c2e15abdfef07119e3cb342ff5c843dccf5c7a
Wallace A <andradewall0@gmail.com>
2024-04-09 23:01:22 -0300

    Initial commit

---
+++ lua/community.lua
@@ -0,0 +1,22 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = { 80, 120 },
+      disabled_filetypes = { "help" },
+      scope = "file",
+    },
+  },
+}
