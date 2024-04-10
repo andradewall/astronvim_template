72904067786f9540b338c420ee2fefd36234f690
Wallace A <andradewall0@gmail.com>
2024-04-09 23:30:42 -0300

    feat: add catppuccin file to enable configs

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      show_end_of_buffer = true,
+      styles = {
+        keywords = { "italic" },
+        loops = { "italic" },
+      },
+    },
+  },
+}
