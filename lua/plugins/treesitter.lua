37d61f06d494fecd5d279be68ff41bf03a489d79
Wallace A <andradewall0@gmail.com>
2024-04-13 15:31:37 -0300

    send configs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Treesitter
 
@@ -10,7 +10,24 @@ return {
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
       "vim",
+      "css",
+      "html",
+      "javascript",
+      "json",
+      "php",
+      "phpdoc",
+      "scss",
       -- add more arguments for adding more treesitter parsers
     })
+
+    opts.auto_install = true
+
+    opts.highlight = {
+      enable = true,
+    }
+
+    opts.indent = {
+      enable = true,
+    }
   end,
 }
